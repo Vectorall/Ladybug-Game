6789c30c58da5633d80db96d6ede6425b54ca474
Vectoral <hectorverduzcorayo@gmail.com>
2022-07-08 10:35:02 -0700

    Window Title

---
+++ conf.lua
@@ -0,0 +1,3 @@
+function love.conf(t)
+    t.window.title = 'Ladybug Game'
+end
--- main.lua
+++ main.lua
@@ -1,13 +1,21 @@
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
+
     ladybugImage = love.graphics.newImage('images/ladybug.png')
+    ladybugX = love.graphics.getWidth() / 2
+    ladybugY = love.graphics.getHeight() / 2
 end
 
 
 function love.draw()
-    love.graphics.setBackgroundColor(96/255, 200/255, 252/255)
+    love.graphics.setBackgroundColor(118/255, 227/255, 158/255)
 
     local ladybugWidth = ladybugImage:getWidth()
     local ladybugHeight = ladybugImage:getHeight()
-    love.graphics.draw(ladybugImage, love.graphics.getWidth() / 2, love.graphics.getHeight() / 2, 0, 2, 2, ladybugWidth / 2, ladybugHeight / 2)
+    love.graphics.draw(ladybugImage, ladybugX, ladybugY, 0, 2, 2, ladybugWidth / 2, ladybugHeight / 2)
+end
+
+
+function love.onmousepressed()
+
 end
